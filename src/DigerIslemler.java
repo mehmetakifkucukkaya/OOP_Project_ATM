7711a81c1a0efadabd1e7327bfa4688da5ec7a66
mehmetakifkucukkaya <mehmetakifkucukkaya@stu.ayvansaray.edu.tr>
2023-01-04 10:06:18 +0300

    SansOyunlari sınıfı tamamlandı

--- src/DigerIslemler.java
+++ src/DigerIslemler.java
@@ -7,8 +7,9 @@ public class DigerIslemler{
     EhliyetIslemleri ehliyet = new EhliyetIslemleri();
     SinavOdemeleri sinav = new SinavOdemeleri();
     FaturaYatir fatura  = new FaturaYatir();
-
     DijitalOyunOdemeleri oyun= new DijitalOyunOdemeleri();
+
+    SansOyunlari sansOyunlari = new SansOyunlari();
     int secilenIslem;
     Scanner scanner = new Scanner(System.in);
     Islemler islemler = new Islemler();
@@ -16,15 +17,16 @@ public class DigerIslemler{
 
         System.out.println(musteri.getBakiye());
 
-        System.out.println("1-)Fatura Ödeme");
-        System.out.println("2-)Sınav Harcı Ödeme");
-        System.out.println("3-)Ehliyet Ödemeleri");
-        System.out.println("4-)Dijital Oyun Ödemeleri");
-        System.out.println("5-)Vergi Ödemeleri");
-        System.out.println("6-)Bağış İşlemleri");
-        System.out.println("7-)Kredi İşlemleri");
-        System.out.println("8-)Döviz İşlemleri");
-        System.out.println("9-)Yatırım/Borsa İşlemleri");
+        System.out.println("1-)  Fatura Ödeme");
+        System.out.println("2-)  Sınav Harcı Ödeme");
+        System.out.println("3-)  Ehliyet Ödemeleri");
+        System.out.println("4-)  Dijital Oyun Ödemeleri");
+        System.out.println("5-)  Vergi Ödemeleri");
+        System.out.println("6-)  Bağış İşlemleri");
+        System.out.println("7-)  Kredi İşlemleri");
+        System.out.println("8-)  Döviz İşlemleri");
+        System.out.println("9-)  Yatırım/Borsa İşlemleri");
+        System.out.println("10-) Şans Oyunları");
 
         System.out.println("\nLütfen yapmak istediğiniz işlemi seçin");
         secilenIslem = scanner.nextInt();
@@ -57,6 +59,9 @@ public class DigerIslemler{
                 break;
             case 9:
                 //yatirimIslemeri();
+                break;
+            case 10:
+                sansOyunlari.sansOyunMenu();
                 break;
             default:
                 System.out.println("Geçersiz işlem seçtiniz! Lütfen tekrar deneyin\n");
--- src/SansOyunlari.java
+++ src/SansOyunlari.java
@@ -1,4 +1,61 @@
-public class SansOyunlari {
+import java.util.Random;
+public class SansOyunlari extends Islemler{
 
+    Random random = new Random();
+    Mesajlar mesaj=new Mesajlar();
+    private int uyeId,secim,kod;
+    private double tutar;
+    public void sansOyunMenu()
+    {
+        System.out.println("Lütfen üye ID'sini giriniz: ");
+        uyeId=scanner.nextInt();
 
+        System.out.println("1-) Milli Piyango");
+        System.out.println("2-) Türkie Jokey Kulübü");
+        System.out.println("3-) Nesine.com");
+
+        System.out.println("Lütfen oyunun kodunu giriniz: ");
+        secim=scanner.nextInt();
+
+        switch (secim)
+        {
+            case 1:
+                odeme("Milli Piyango");
+                mesaj.islemSonuMesaji();
+                break;
+            case 2: odeme("Türkiye Jokey Kulübü");
+                mesaj.islemSonuMesaji();
+                break;
+            case 3: odeme("Nesine.com");
+                mesaj.islemSonuMesaji();
+                break;
+            default:
+                System.out.println("Geçersiz Giriş Lütfen Tekrar deneyin");
+                sansOyunMenu();
+                break;
+        }
+
+    }
+
+    public void odeme(String sansOyunu)
+    {
+        System.out.println("Lütfen Tutarı Giriniz: ");
+        tutar=scanner.nextDouble();
+
+        if(bakiye >= tutar)
+        {
+            bakiye-=tutar;
+            kod=random.nextInt(10000);
+        }
+        else
+        {
+            mesaj.yetersizBakiyeMesaji();
+        }
+
+        System.out.println(tutar +" değerinde "+ sansOyunu + " kupununuz tanımlanmıştır");
+        System.out.println("Kupon kodunuz: "+ kod);
+
+        System.out.println("Mevcut Bakiyeniz: "+bakiye);
+    }
 }
+
